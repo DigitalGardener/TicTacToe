f88b4506bd68db596830b3cdeb03a3c8a9228d0c
DigitalGardener <lyndon.mclaren@gmail.com>
2015-09-24 16:26:20 -0500

    Added minmal initial model with simplest initial test

---
+++ src/TicTacToe/TicTacToe.Domain/Game.cs
@@ -0,0 +1,13 @@
+﻿namespace TicTacToe.Domain
+{
+    /// <summary>
+    /// Represents a Tic-Tac-Toe game
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// Indicates whether player's can continue to play
+        /// </summary>
+        public bool IsOver { get; } = false;
+    }
+}
