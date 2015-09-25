4220dac79625ff008e533fac6da95d2b322af32b
DigitalGardener <lyndon.mclaren@gmail.com>
2015-09-25 15:36:31 -0500

    Modeled game cells and created test for their initial state

---
+++ src/TicTacToe/TicTacToe.Domain/CellStatus.cs
@@ -0,0 +1,21 @@
+﻿namespace TicTacToe.Domain
+{
+    /// <summary>
+    /// Represents cell status
+    /// </summary>
+    public enum CellStatus
+    {
+        /// <summary>
+        /// Cell has been marked by one player
+        /// </summary>
+        X,
+        /// <summary>
+        /// Cell has been marked by the other player
+        /// </summary>
+        O,
+        /// <summary>
+        /// Cell has not been marked by either player
+        /// </summary>
+        Unmarked
+    }
+}
--- src/TicTacToe/TicTacToe.Domain/Game.cs
+++ src/TicTacToe/TicTacToe.Domain/Game.cs
@@ -1,13 +1,39 @@
-﻿namespace TicTacToe.Domain
+﻿using System.Collections.Generic;
+using static System.Linq.Enumerable;
+
+namespace TicTacToe.Domain
 {
     /// <summary>
     /// Represents a Tic-Tac-Toe game
     /// </summary>
     public class Game
     {
+
+        public const int GRID_SIZE = 3;
+        private CellStatus[,] _cells = new CellStatus[GRID_SIZE, GRID_SIZE];
+
+        /// <summary>
+        /// Indicates the current status of all cells
+        /// </summary>
+        public IEnumerable<CellStatus> CellStatuses => _cells.Cast<CellStatus>();
+
         /// <summary>
         /// Indicates the current state of play
         /// </summary>
         public GameStatus Status { get; private set; } = GameStatus.New;
+
+        /// <summary>
+        /// Initializes a new Game with unmarked cells
+        /// </summary>
+        public Game()
+        {
+            for (int row = 0; row < GRID_SIZE; row++)
+            {
+                for (int column = 0; column < GRID_SIZE; column++)
+                {
+                    _cells[row, column] = CellStatus.Unmarked;
+                }
+            }
+        }
     }
 }
